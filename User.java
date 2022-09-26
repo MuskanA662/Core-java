1f84d8653a5de2b86465a05cdce616eec59ffca2
MuskanA662 <muskana.xworkz@gmail.com>
2022-09-26 16:04:31 +0530

    array

---
+++ User.java
@@ -0,0 +1,13 @@
+class User{
+  static String cards[]= {"Platinum Credit card","Millenia credit/debit card","Kotak urban card"};
+   public static void main(String a[]){
+
+	
+
+	   for(int b = 0 ; b < cards.length ; b++)
+		{
+	System.out.println (cards[b]);
+
+		}
+}
+}
---
+++ superMarket.java
@@ -0,0 +1,70 @@
+class SuperMarket
+{
+	static String SuperMarketName = "Reliance Mart";
+	//static String  vegetables[];
+    public static void main(String a[])
+	{ 
+	System.out.println("Welcome to " + SuperMarketName);
+	
+	
+	String vegetables []= {"Potato","Tomato","Heeri kai","Califlower","Brinjal","Cucumber","Drumstick","Onion","Coriander Leaves","Green chillies","Lemon"};
+	System.out.println("\nList of vegetables");
+	System.out.println(vegetables[0]+"\n"+ vegetables[1]+"\n"+vegetables[2]+"\n"+vegetables[3]+"\n"+vegetables[4]+"\n"+vegetables[5]+"\n"+vegetables[6]+"\n"+vegetables[7]+"\n"+vegetables[8]+"\n"+vegetables[9]+"\n"+vegetables[10]);
+  
+  
+	String perfumes []= {"What a girl","Fogg","Engauge","Secret","Dove","Diamond"};
+	System.out.println("\nList of perfumes");
+	System.out.println(perfumes[0]+"\n"+ perfumes[1]+"\n"+perfumes[2]+"\n"+perfumes[3]+"\n"+perfumes[4]+"\n"+perfumes[5]);
+ 
+	String snacks []= {"chips","kurkure","Biscuits","Mungdal","Chocolates","noodles","papad"};
+	System.out.println("\nList of Snacks");
+	System.out.println(snacks[0]+"\n"+ snacks[1]+"\n"+snacks[2]+"\n"+snacks[3]+"\n"+snacks[4]+"\n"+snacks[5]+"\n"+snacks[6]);
+ 
+	String cosmetics []= {"Lipstick","Foundation","Compact","Eyeliner","Mascara","Lipbalm","Kajal","face wash","Toner","Moisturizer","Sunscreen","Powder","Primer","Concealer","Lipgloss","Corrector","Nail polish"};
+	System.out.println("\nList of cosmetics");
+	System.out.println(cosmetics[0]+"\n"+ cosmetics[1]+"\n"+cosmetics[2]+"\n"+cosmetics[3]+"\n"+cosmetics[4]+"\n"+cosmetics[5]+"\n"+cosmetics[6]+"\n"+cosmetics[7]+"\n"+cosmetics[8]+"\n"+cosmetics[9]+"\n"+cosmetics[10]+"\n"+cosmetics[11]+"\n"+cosmetics[12]+"\n"+cosmetics[13]+"\n"+cosmetics[14]+"\n"+cosmetics[15]+"\n"+cosmetics[16]);
+  
+    String chocolates []= {"Dark chocolate","Kit-kat","Dairymilk","Munch","5-star","Kachha mango","Eclairs","alphenlibey","Nuts","Choco Temptation"};
+	System.out.println("\nList of chocolates");
+    System.out.println(chocolates[0]+"\n"+ chocolates[1]+"\n"+chocolates[2]+"\n"+chocolates[3]+"\n"+chocolates[4]+"\n"+chocolates[5]+"\n"+chocolates[6]+"\n"+chocolates[7]+"\n"+chocolates[8]+"\n"+chocolates[9]);
+	
+		
+	String kitchenUtensils []= {"Teapot","Aprin","Grinder","Bowl","Pan","Knife","kettle","Cup","Plates","Spoons"};
+	System.out.println("\nList of kitchenUtensils");
+	System.out.println(kitchenUtensils[0]+"\n"+ kitchenUtensils[1]+"\n"+kitchenUtensils[2]+"\n"+kitchenUtensils[3]+"\n"+kitchenUtensils[4]+"\n"+kitchenUtensils[5]+"\n"+kitchenUtensils[6]+"\n"+kitchenUtensils[7]+"\n"+kitchenUtensils[8]+"\n"+kitchenUtensils[9]);
+	
+	String soaps []= {"Dove","Santoor","Pears","Medimix","Lifeboy","Lux","Rexona","Wildstone","Liril","Hamam"};
+	System.out.println("\nList of soaps");
+	System.out.println(soaps[0]+"\n"+ soaps[1]+"\n"+soaps[2]+"\n"+soaps[3]+"\n"+soaps[4]+"\n"+soaps[5]+"\n"+soaps[6]+"\n"+soaps[7]+"\n"+soaps[8]+"\n"+soaps[9]);
+	
+	String footWears []= {"Heels","Boots","Crocks","Knee high boots","Pump","Ballerinas","Slippers","Rain boots","Sneakers","Uggs"};
+	System.out.println("\nList of footWears");
+	System.out.println(footWears[0]+"\n"+ footWears[1]+"\n"+footWears[2]+"\n"+footWears[3]+"\n"+footWears[4]+"\n"+footWears[5]+"\n"+footWears[6]+"\n"+footWears[7]+"\n"+footWears[8]+"\n"+footWears[9]);
+	
+	String biscuits []= {"Marrie Gold","Oreo","Parle-G","Tiger","Hide and Seek","Horlicks","Nutri choice","Monaco","Burbon","Bournvita"};
+	System.out.println("\nList of biscuits");
+	System.out.println(biscuits[0]+"\n"+ biscuits[1]+"\n"+biscuits[2]+"\n"+biscuits[3]+"\n"+biscuits[4]+"\n"+biscuits[5]+"\n"+biscuits[6]+"\n"+biscuits[7]+"\n"+biscuits[8]+"\n"+biscuits[9]);
+	
+	String oils []= {"Sunrich","Fortune","Dhara","Gemini","AAdhar","Freedom","Safola","Sunflower","Organic coconut oil","Organic sun flower oil"};
+	System.out.println("\nList of oils");
+	System.out.println(oils[0]+"\n"+ oils[1]+"\n"+oils[2]+"\n"+oils[3]+"\n"+oils[4]+"\n"+oils[5]+"\n"+oils[6]+"\n"+oils[7]+"\n"+oils[8]+"\n"+oils[9]);
+	
+	String meats []= {"Chiken","Mutton","Fish","Beef","Duck","Lobsters","Oysters","Pork","Vanison","Lamb"};
+	System.out.println("\nList of meats");
+	System.out.println(meats[0]+"\n"+ meats[1]+"\n"+meats[2]+"\n"+meats[3]+"\n"+meats[4]+"\n"+meats[5]+"\n"+meats[6]+"\n"+meats[7]+"\n"+meats[8]+"\n"+meats[9]);
+	
+	String electronics []= {"Earphones","Charger","Connectors","Pendrives","Simcards","Hair drier","Television","Computers","Shaving Machine","Smart watch"};
+    System.out.println("\nList of electronics");
+	System.out.println(electronics[0]+"\n"+ electronics[1]+"\n"+electronics[2]+"\n"+electronics[3]+"\n"+electronics[4]+"\n"+electronics[5]+"\n"+electronics[6]+"\n"+electronics[7]+"\n"+electronics[8]+"\n"+electronics[9]);
+	
+	String beverages [] = {"Maaza","Sprite","Coco-cola","Mountain Dew","Frootie","Lassi","Jeerasida","Beer","Limca","Mirrinda"};
+    System.out.println("\nList of beverages");
+	System.out.println(beverages[0]+"\n"+ beverages[1]+"\n"+beverages[2]+"\n"+beverages[3]+"\n"+beverages[4]+"\n"+beverages[5]+"\n"+beverages[6]+"\n"+beverages[7]+"\n"+beverages[8]+"\n"+beverages[9]);
+	
+	String grains [] = {"Wheat Berries","Rice","Brown Rice","Barley","Corn","Oatmeal","Kaniva","Bulgur","Spelt","Rye"};
+    System.out.println("\nList of grains");
+	System.out.println(grains[0]+"\n"+ grains[1]+"\n"+grains[2]+"\n"+grains[3]+"\n"+grains[4]+"\n"+grains[5]+"\n"+grains[6]+"\n"+grains[7]+"\n"+grains[8]+"\n"+grains[9]);
+  
+  }
+ 
+}
