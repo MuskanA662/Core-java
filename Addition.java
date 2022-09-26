aa0431ef523e5f86583c96e70d4b6dbee96ce261
MuskanA662 <muskana.xworkz@gmail.com>
2022-09-26 16:26:11 +0530

    Java Assignment

---
+++ Addition.java
@@ -0,0 +1,15 @@
+class Addition{
+
+      public static void main(String args[]){
+	  
+	  int a = 23;
+	  int b = 14;
+	  int c;
+	  
+	  c = (a-(-b));
+	  
+	  System.out.println("Addition of two numbers without using olus operator is" + c );
+	  
+	}
+ 
+}
---
+++ Prime.java
@@ -0,0 +1,18 @@
+class Prime
+{
+	public static void main(String args[])
+	{
+		int n = 11;
+		if(n % 2 == 0)
+		{
+			System.out.println("The number is not prime");
+	    }
+		else{
+	     System.out.println("The number is prime");
+	  
+	  }
+	  
+	  
+	}
+
+}
