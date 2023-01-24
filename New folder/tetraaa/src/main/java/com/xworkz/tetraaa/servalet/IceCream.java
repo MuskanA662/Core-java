d3d7b0676149b5806d4be94b199d265896399ec7
MuskanA662 <muskana.xworkz@gmail.com>
2023-01-24 23:13:45 +0530

    adding servalet files

---
+++ New folder/tetraaa/src/main/java/com/xworkz/tetraaa/servalet/IceCream.java
@@ -0,0 +1,24 @@
+package com.xworkz.tetraaa.servalet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup=4,urlPatterns="/carnetto")
+public class IceCream extends HttpServlet{
+	public IceCream() {
+		System.out.println("creating"+this.getClass().getSimpleName());
+	}
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException{
+		System.out.println("running doGet in IcecreamServlet");
+	}
+
+
+}
